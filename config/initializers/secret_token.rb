df9c7bb81e9766524b467c8d55fbf08110489261
Anders Emil Nielsen <andersemil91@gmail.com>
2012-11-07 16:27:32 +0100

    Rails project added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviZ::Application.config.secret_token = 'f139870312a9557d2acaedfb23f379467687624510d8a4d503f5270c7273db770c1299c1821cef781435cd5c8bd4317ac3426d407d32f909673f44422d24ce2a'
