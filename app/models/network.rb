fc96cd03dfab8e4010557359a31b22f733541c79
Anders Emil Nielsen <andersemil91@gmail.com>
2012-11-25 20:52:33 +0100

    It should be possible to add edges through a network

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,11 @@
 class Network < ActiveRecord::Base
   attr_accessible :id
+
+  has_many :edges
+
+  def add_edges_to_related(source_id, target_ids)
+    target_ids.each do |target_id|
+      edges << Edge.new(source_id: source_id, target_id: target_id)
+    end
+  end
 end
--- app/services/movie_network_service.rb
+++ app/services/movie_network_service.rb
@@ -2,7 +2,7 @@ class MovieNetworkService
   class << self
     def find_related_movies(movie_id)
       related_movies = Movie.find_related(movie_id)
-      current_network.add_edges_to_related(movie_id, related_movies)
+      current_network.add_edges_to_related(movie_id, related_movies.map(&:id))
     end
   end
 end
