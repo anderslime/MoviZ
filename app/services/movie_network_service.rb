711eb92678560bab4c5abcd3fa1b0f1264cefa91
Anders Emil Nielsen <andersemil91@gmail.com>
2012-11-26 22:01:50 +0100

    stuff

--- app/services/movie_network_service.rb
+++ app/services/movie_network_service.rb
@@ -1,12 +1,14 @@
 class MovieNetworkService
   class << self
     def find_related_movies(movie_id)
-      related_movies = Movie.find_related(movie_id)
-      current_network.add_edges_to_related(movie_id, related_movies.map(&:id))
+      related_nodes = NetworkMovieRelations.find_relations(params[:id])
+      edges = current_network.add_edges_to_related(movie_id, related_nodes.map(&:id))
+      nodes, edges
     end
 
     def find_source_movie(title)
       movie = Movie.find_by_title(title)
+      current_network.add_node(movie)
       MovieNetworkService.find_related_movies(movie.id, movie.related.map(&:id))
     end
   end
