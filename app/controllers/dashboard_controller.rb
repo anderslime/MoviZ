08be2b1d9a93adb63584a4b35895d20c22e7c95b
Anders Emil Nielsen <andersemil91@gmail.com>
2012-11-29 13:39:16 +0100

    A little change

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
 class DashboardController < ApplicationController
-  layout :resolve_layout
-
   def index
   end
 end
--- app/services/movie_relation_service.rb
+++ app/services/movie_relation_service.rb
@@ -8,25 +8,22 @@ class MovieRelationService < Struct.new(:movie, :network)
   end
 
   # Returns the MIN_NUMBER_OF_RELATIONS best rated related movies, plus all movies with relations to other existing movies
-  def find_best_related_movies    
+  def find_best_related_movies
     [].tap do |related_movies|
       sorted_movie_relation_ids.each_with_index do |movie_id, index|
-        next if index >= MIN_NUMBER_OF_RELATIONS && !network_node_ids.include?(movie_id)
-        related_movies << Movie.find(movie_id)
+        related_movies << Movie.find(movie_id) if movie_is_relevant?(movie_id, index)
       end
     end
   end
 
   private
 
-  # Returns all ids for nodes in the current network
-  def network_node_ids
-    @network_nodes = network.node_ids
+  def movie_is_relevant?(movie_id, index)
+    index < MIN_NUMBER_OF_RELATIONS || network.node_ids.include?(movie_id)
   end
 
   # Returns all ids for related movies sorted by ratung
   def sorted_movie_relation_ids
     @relations ||= movie.related.sort {|a,b| b[:rating] <=> a[:rating]}.map{|m| m[:movie_id]}
   end
-  
 end
