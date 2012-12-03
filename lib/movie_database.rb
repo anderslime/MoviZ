72b8837c67d8d7c124c7567bd1d4eb6b209d4947
Anders Emil Nielsen <andersemil91@gmail.com>
2012-12-03 19:18:21 +0100

    Removed non working TMDB

--- lib/movie_database.rb
+++ lib/movie_database.rb
@@ -6,19 +6,21 @@ class MovieDatabase
     def current_database=(database)
       @@current_database = database.is_a?(MovieDatabase) ? database : "MovieDatabases::#{database.to_s.classify}".constantize
     end
-    
+
     def current_database
       @@current_database ||= MovieDatabases::Rovi
     end
-    
+
     delegate :find_by_title, :to => MovieDatabases::Rovi
-    
+
     def find_by_id(title, *args)
       rovi_data = MovieDatabases::Rovi.find_by_id(title, *args)
-      tmdb_data = MovieDatabases::TMDB.find_by_id(title, *args)
-      
-      rovi_data.merge(tmdb_data)
+
+      # TMDB Not working yet
+      #tmdb_data = MovieDatabases::TMDB.find_by_id(title, *args)
+      #rovi_data.merge(tmdb_data)
+
+      rovi_data
     end
-    
   end
 end
