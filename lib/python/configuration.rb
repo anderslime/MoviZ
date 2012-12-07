1a7218f59c62e7034425795768efeef0ec1b372b
Anders Emil Nielsen <andersemil91@gmail.com>
2012-12-07 11:56:59 +0100

    Trying with python 2.7

--- lib/python/configuration.rb
+++ lib/python/configuration.rb
@@ -5,7 +5,7 @@ module Python
     class << self
       def initialize
         # start the Python VM
-        RubyPython.start(:python_exe => "python2.6")
+        RubyPython.start(:python_exe => "python2.7")
 
         # Add this folder as search path
         sys = RubyPython.import('sys')
